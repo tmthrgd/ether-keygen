4044259b9fa1894cd577879a8d69259dc162b851
Tom Thorogood <me+github@tomthorogood.co.uk>
2016-03-08 20:44:26 +1030

    Add boilerplate implementation

---
+++ keygen.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"crypto/rand"
+	"time"
+
+	serf "github.com/hashicorp/serf/client"
+)
+
+const (
+	// tick = 1 * time.Hour
+	tick = 5 * time.Second
+
+	ahead  = 2
+	behind = 26
+
+	total = ahead + 1 + behind
+
+	nameLen = 16
+	encLen  = 16
+	hmacLen = 16
+	keySize = nameLen + encLen + hmacLen
+)
+
+var keys = make([][keySize]byte, 0, total)
+
+func main() {
+	conf := &serf.Config{Addr: "127.0.0.1:7373"}
+
+	// TODO: conf.Addr
+	// TODO: conf.AuthKey
+	// TODO: conf.Timeout
+
+	rpc, err := serf.ClientFromConfig(conf)
+	if err != nil {
+		panic(err)
+	}
+
+	// TODO: rpc.Join([]string{}, false)
+
+	// TODO: retrieve old keys from network...
+
+	for i := 0; i < ahead+1; i++ {
+		var key [keySize]byte
+
+		if _, err = rand.Read(key[:]); err != nil {
+			panic(err)
+		}
+
+		if err = rpc.UserEvent("install-key", key[:], false); err != nil {
+			panic(err)
+		}
+
+		keys = append([][keySize]byte{key}, keys...)
+	}
+
+	// TODO: wait
+
+	if err = rpc.UserEvent("use-key", keys[ahead][:nameLen], false); err != nil {
+		panic(err)
+	}
+
+	for range time.Tick(tick) {
+		var key [keySize]byte
+
+		if _, err = rand.Read(key[:]); err != nil {
+			panic(err)
+		}
+
+		if err = rpc.UserEvent("install-key", key[:], false); err != nil {
+			panic(err)
+		}
+
+		if len(keys) == total {
+			if err = rpc.UserEvent("remove-key", keys[total-1][:nameLen], false); err != nil {
+				panic(err)
+			}
+
+			// zero old key
+			for i := 0; i < keySize; i++ {
+				keys[total-1][i] = 0
+			}
+
+			keys = append([][keySize]byte{key}, keys[:total-1]...)
+		} else {
+			keys = append([][keySize]byte{key}, keys...)
+		}
+
+		if err = rpc.UserEvent("use-key", keys[ahead][:nameLen], false); err != nil {
+			panic(err)
+		}
+	}
+}
